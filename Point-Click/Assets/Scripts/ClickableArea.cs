d2194f7f9bc4afabd3556f3985f8209d8985a551
Wilhelm Eriksson <wilhelm.eriksson04@gmail.com>
2026-05-05 11:17:17 +0200

    restartOver

--- Point-Click/Assets/Scripts/ClickableArea.cs
+++
@@ -1,6 +0,0 @@
-using UnityEngine;
-
-// Marker component — attach to any GameObject with a Collider to make it a walkable area.
-// The FirstPersonController raycasts for this component on hit to decide where to move.
-[RequireComponent(typeof(Collider))]
-public class ClickableArea : MonoBehaviour { }
